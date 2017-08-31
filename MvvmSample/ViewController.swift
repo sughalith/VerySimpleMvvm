69fb1dae049483c7d6ebfdd8511a4bcd94cc6886
Arkadiusz Stankiewicz <a.stankiewicz@actumlab.com>
2017-08-31 10:16:29 +0200

    Initial Commit

---
+++ MvvmSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MvvmSample
+//
+//  Created by mini2010 on 31.08.2017.
+//  Copyright © 2017 ActumLab Structure Department. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
